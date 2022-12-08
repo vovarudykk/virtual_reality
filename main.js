37b2abcab71a9341fcc9bd53a7d02be41b23a27b
vovarudykk <rudykviv@gmail.com>
2022-12-09 00:06:31 +0200

    lab2 try #9999

--- main.js
+++ main.js
@@ -5,37 +5,24 @@ let surface; // A surface model
 let shProgram; // A shader program
 let spaceball; // A SimpleRotator object that lets the user rotate the view by mouse.
 
-let splines = 50;
-// const a = 6;
-// const b = 15;
+let handlePosition = 0.0;
 
-function deg2rad(angle) {
+let a = 6;
+let b = 20;
+
+const deg2rad = (angle) => {
   return (angle * Math.PI) / 180;
-}
-
-// const r = (z) => {
-//   return z * Math.sqrt((z * (a - z)) / b);
-// };
-
-// const x = (z, beta) => {
-//   return r(z) * Math.sin(beta);
-// };
-
-// const y = (z, beta) => {
-//   return r(z) * Math.cos(beta);
-// };
+};
 
 // Constructor
 function Model(name) {
   this.name = name;
   this.iVertexBuffer = gl.createBuffer();
-
   this.count = 0;
 
   this.BufferData = function (vertices) {
     gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
-
     this.count = vertices.length / 3;
   };
 
@@ -44,12 +31,10 @@ function Model(name) {
     gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(shProgram.iAttribVertex);
 
-    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
+    gl.vertexAttribPointer(shProgram.iNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(shProgram.iNormal);
 
-    for (let i = 0; i < (splines + 1) * 2; i++) {
-      gl.drawArrays(gl.TRIANGLE_STRIP, i * splines, splines);
-      // gl.drawArrays(gl.LINE_STRIP, i * splines, splines);
-    }
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
   };
 }
 
@@ -65,6 +50,18 @@ function ShaderProgram(name, program) {
   // Location of the uniform matrix representing the combined transformation.
   this.iModelViewProjectionMatrix = -1;
 
+  this.iNormal = -1;
+  this.iNormalMatrix = -1;
+
+  this.iAmbientColor = -1;
+  this.iDiffuseColor = -1;
+  this.iSpecularColor = -1;
+
+  this.iShininess = -1;
+
+  this.iLightPosition = -1;
+  this.iLightVec = -1;
+
   this.Use = function () {
     gl.useProgram(this.prog);
   };
@@ -79,16 +76,18 @@ function draw() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   /* Set the values of the projection transformation */
-  let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
+  let projection = m4.perspective(Math.PI / 3, 1, 1, 1000);
 
   /* Get the view matrix from the SimpleRotator object.*/
   let modelView = spaceball.getViewMatrix();
 
-  let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
-  let translateToPointZero = m4.translation(0, 0, -10);
+  let rotateToPointZero = m4.axisRotation([1, 0, 0], 1);
+  let translateToPointZero = m4.translation(0, 2, -10);
 
   let matAccum0 = m4.multiply(rotateToPointZero, modelView);
   let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
+  const modelViewInverse = m4.inverse(matAccum1, new Float32Array(16));
+  const normalMatrix = m4.transpose(modelViewInverse, new Float32Array(16));
 
   /* Multiply the projection matrix times the modelview matrix to give the
        combined transformation matrix, and send that to the shader program. */
@@ -99,53 +98,102 @@ function draw() {
     false,
     modelViewProjection
   );
+  gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
 
+  gl.uniform3fv(shProgram.iLightPosition, lightCoordinates());
+  gl.uniform3fv(shProgram.iLightDirection, [1, 0, 0]);
+
+  gl.uniform3fv(shProgram.iLightVec, new Float32Array(3));
+
+  gl.uniform1f(shProgram.iShininess, 1.0);
+
+  gl.uniform3fv(shProgram.iAmbientColor, [0.5, 0, 0.4]);
+  gl.uniform3fv(shProgram.iDiffuseColor, [1.3, 1.0, 0.0]);
+  gl.uniform3fv(shProgram.iSpecularColor, [1.5, 1.0, 1.0]);
   /* Draw the six faces of a cube, with different colors. */
   gl.uniform4fv(shProgram.iColor, [1, 1, 0, 1]);
 
   surface.Draw();
 }
 
-function CreateSurfaceData() {
-  let a = 0.5;
-  let b = 1;
+const r = (z) => {
+  return z * Math.sqrt((z * (a - z)) / b);
+};
 
+const x = (z, beta) => {
+  return r(z) * Math.sin(beta);
+};
+
+const y = (z, beta) => {
+  return r(z) * Math.cos(beta);
+};
+
+const step = (max, splines = 20) => {
+  return max / (splines - 1);
+};
+
+function CreateSurfaceData() {
   let vertexList = [];
 
-  for (let u = 0; u <= 180; u += 180 / (splines - 1)) {
-    for (let v = 0; v <= 360; v += 360 / (splines - 1)) {
-      vertexList.push(
-        a *
-          (b - Math.cos(deg2rad(u))) *
-          Math.sin(deg2rad(u)) *
-          Math.cos(deg2rad(v)),
-        a *
-          (b - Math.cos(deg2rad(u))) *
-          Math.sin(deg2rad(u)) *
-          Math.sin(deg2rad(v)),
-        Math.cos(deg2rad(u))
-      );
-    }
-  }
+  let splines = 32;
 
-  for (let v = 0; v <= 360; v += 360 / (splines - 1)) {
-    for (let u = 0; u <= 180; u += 180 / (splines - 1)) {
+  let zMax = a;
+  let betaMax = 2 * Math.PI;
+  let zStep = step(zMax, splines);
+  let betaStep = step(betaMax, splines);
+
+  let upSurface = 0;
+
+  for (let z = 0; z <= a; z += zStep) {
+    for (let beta = 0; beta <= 2 * Math.PI; beta += betaStep) {
+      vertexList.push(x(r(z), beta), y(r(z), beta), z + upSurface);
       vertexList.push(
-        a *
-          (b - Math.cos(deg2rad(u))) *
-          Math.sin(deg2rad(u)) *
-          Math.cos(deg2rad(v)),
-        a *
-          (b - Math.cos(deg2rad(u))) *
-          Math.sin(deg2rad(u)) *
-          Math.sin(deg2rad(v)),
-        Math.cos(deg2rad(u))
+        x(r(z + zStep), beta + betaStep),
+        y(r(z + zStep), beta + betaStep),
+        z + zStep + upSurface
       );
     }
   }
 
   return vertexList;
 }
+
+// const cos = (x) => {
+//   return Math.cos(x);
+// };
+
+// const sin = (x) => {
+//   return Math.sin(x);
+// };
+
+// function CreateSurfaceData() {
+//   let vertexList = [];
+//   let splines = 20;
+
+//   let maxU = Math.PI;
+//   let maxV = 2 * Math.PI;
+//   let stepU = step(maxU, splines);
+//   let stepV = step(maxV, splines);
+
+//   console.log(`maxU=${maxU}|stepU=${stepU}`);
+//   console.log(`maxV=${maxV}|stepV=${stepV}`);
+
+//   for (let u = 0; u <= maxU; u += stepU) {
+//     for (let v = 0; v <= maxV; v += stepV) {
+//       vertexList.push(
+//         a * (b - cos(u)) * sin(u) * cos(v),
+//         a * (b - cos(u)) * sin(u) * sin(v),
+//         cos(u)
+//       );
+//       vertexList.push(
+//         a * (b - cos(u + stepU)) * sin(u + stepU) * cos(v + stepV),
+//         a * (b - cos(u + stepU)) * sin(u + stepU) * sin(v + stepV),
+//         cos(u + stepU)
+//       );
+//     }
+//   }
+//   return vertexList;
+// }
 
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
@@ -160,6 +208,18 @@ function initGL() {
     "ModelViewProjectionMatrix"
   );
   shProgram.iColor = gl.getUniformLocation(prog, "color");
+
+  shProgram.iNormal = gl.getAttribLocation(prog, "normal");
+  shProgram.iNormalMatrix = gl.getUniformLocation(prog, "normalMatrix");
+
+  shProgram.iAmbientColor = gl.getUniformLocation(prog, "ambientColor");
+  shProgram.iDiffuseColor = gl.getUniformLocation(prog, "diffuseColor");
+  shProgram.iSpecularColor = gl.getUniformLocation(prog, "specularColor");
+
+  shProgram.iShininess = gl.getUniformLocation(prog, "shininess");
+
+  shProgram.iLightPosition = gl.getUniformLocation(prog, "lightPosition");
+  shProgram.iLightVec = gl.getUniformLocation(prog, "lightVec");
 
   surface = new Model("Surface");
   surface.BufferData(CreateSurfaceData());
@@ -228,3 +288,36 @@ function init() {
 
   draw();
 }
+
+window.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "ArrowLeft":
+      left();
+      break;
+    case "ArrowRight":
+      right();
+      break;
+    default:
+      return;
+  }
+});
+
+const left = () => {
+  handlePosition -= 0.07;
+  reDraw();
+};
+
+const right = () => {
+  handlePosition += 0.07;
+  reDraw();
+};
+
+const lightCoordinates = () => {
+  let coord = Math.sin(handlePosition) * 1.2;
+  return [coord, -2, coord * coord];
+};
+
+const reDraw = () => {
+  surface.BufferData(CreateSurfaceData());
+  draw();
+};
--- vertexShader.glsl
+++ vertexShader.glsl
@@ -3,15 +3,37 @@ const vertexShaderSource = `
 
 attribute vec3 vertex;
 attribute vec3 normal;
-uniform mat3 normalMatrix;
+uniform mat4 normalMatrix;
 uniform mat4 ModelViewProjectionMatrix;
 
-varying vec3 v_position;
-varying vec3 v_normal;
+uniform float shininess;
+uniform vec3 ambientColor;
+uniform vec3 diffuseColor;
+uniform vec3 specularColor;
+
+uniform vec3 lightPosition;
+
+varying vec4 color;
 
 void main() {
-    v_position = vec3(0,0,0);
-    v_normal = normalize(normalMatrix * normal);
+    vec4 vertexPosition4 = ModelViewProjectionMatrix * vec4(vertex, 1.0);
+    vec3 vertexPosition = vec3(vertexPosition4) / vertexPosition4.w;
+    vec3 normalInterpolation = vec3(normalMatrix * vec4(normal, 0.0));
+    gl_Position = vertexPosition4;
     
-    gl_Position = ModelViewProjectionMatrix * vec4(vertex,1.0);
+    vec3 normal = normalize(normalInterpolation);
+    vec3 lightDirection = normalize(lightPosition - vertexPosition);
+    
+    float nDotLight = max(dot(normal, lightDirection), 0.0);
+    float specularLight = 0.0;
+    if (nDotLight > 0.0) {
+        vec3 viewDirection = normalize(-vertexPosition);
+        vec3 halfDirection = normalize(lightDirection + viewDirection);
+        float specularAngle = max(dot(halfDirection, normal), 0.0);
+        specularLight = pow(specularAngle, shininess);
+    }
+    vec3 diffuse = nDotLight * diffuseColor;
+    vec3 ambient = ambientColor;
+    vec3 specular = specularLight * specularColor;
+    color = vec4(diffuse + ambient + specular, 1.0);
 }`;
