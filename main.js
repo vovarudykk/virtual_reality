4794865f410664dd4ed19df92e3aca149697ad33
vovarudykk <rudykviv@gmail.com>
2023-05-26 11:29:11 +0300

    audio panner and filter, create sphere

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ let webCamTexture, webCamVideo, webCamBackground;
 
 let orientationEvent = { alpha: 0, beta: 0, gamma: 0 };
 
+let sphere, textureSphere;
+
+let audio = null;
+let audioContext, audioSource, audioPanner, audioFilter, cutoffFrequency;
+
+let xPosition = 0;
+let yPosition = 0;
+let zPosition = 0;
+
 let a = 0.5;
 let b = 1;
 
@@ -101,13 +110,14 @@ function draw() {
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+  xPosition = parseFloat(document.getElementById("xPosition").value);
+  yPosition = parseFloat(document.getElementById("yPosition").value);
+  zPosition = parseFloat(document.getElementById("zPosition").value);
+
+  let projection = m4.orthographic(0, 1, 0, 1, -1, 1);
+
   let modelView = spaceball.getViewMatrix();
   let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0);
-  let projection = m4.orthographic(0, 1, 0, 1, -1, 1);
-  let noRot = m4.multiply(
-    rotateToPointZero,
-    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-  );
 
   const stereoCamera = {
     eyeSeparation: parseFloat(document.getElementById("eyeSeparation").value),
@@ -118,12 +128,12 @@ function draw() {
     far: 100.0,
   };
 
-  let projectionLeft = leftFrustum(stereoCamera);
-  let projectionRight = rightFrustum(stereoCamera);
+  let noRot = m4.multiply(
+    rotateToPointZero,
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+  );
 
-  let translateToLeft = m4.translation(-0.03, 0, -20);
-  let translateToRight = m4.translation(0.03, 0, -20);
-
+  // draw background
   gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, noRot);
   gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projection);
   gl.bindTexture(gl.TEXTURE_2D, webCamTexture);
@@ -140,6 +150,12 @@ function draw() {
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.clear(gl.DEPTH_BUFFER_BIT);
 
+  let modelView1 = null;
+
+  // draw sphere
+
+  gl.bindTexture(gl.TEXTURE_2D, textureSphere);
+
   if (
     orientationEvent.alpha &&
     orientationEvent.beta &&
@@ -152,12 +168,56 @@ function draw() {
     );
     let translationMatrix = m4.translation(0, 0, -1);
 
-    modelView = m4.multiply(rotationMatrix, translationMatrix);
+    xPosition = orientationEvent.gamma;
+    yPosition = orientationEvent.beta;
+    // zPosition = orientationEvent.alpha;
+
+    modelView1 = m4.multiply(rotationMatrix, translationMatrix);
   }
 
-  // console.log(JSON.stringify(rotationMatrix));
+  if (audioPanner) {
+    cutoffFrequency = document.getElementById("cutoffFrequency");
+    audioFilter.frequency.value = parseFloat(cutoffFrequency.value);
+    // high value for change panner position slower
+    let pannerCoef = 0.7;
+
+    audioPanner.setPosition(
+      xPosition * pannerCoef,
+      yPosition * pannerCoef,
+      zPosition
+    );
+  }
+
+  let translateToPointZero = m4.translation(50, 50, 0);
+  const translationMatrix = m4.translation(xPosition, yPosition, zPosition);
+  let matAccumSphere = m4.multiply(
+    rotateToPointZero,
+    modelView1 ? modelView1 : modelView
+  );
+  let translationMatrixSphere = m4.multiply(translationMatrix, matAccumSphere);
+
+  const scaleMatrix = m4.scaling(0.01, 0.01, 0.01);
+
+  const matAccumSphere0 = m4.multiply(
+    scaleMatrix,
+    m4.multiply(translateToPointZero, translationMatrixSphere)
+  );
+  gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projection);
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, matAccumSphere0);
+  sphere.Draw();
+  gl.clear(gl.DEPTH_BUFFER_BIT);
+
+  // draw surface
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
 
   let matAccum = m4.multiply(rotateToPointZero, modelView);
+
+  let projectionLeft = leftFrustum(stereoCamera);
+  let projectionRight = rightFrustum(stereoCamera);
+
+  let translateToLeft = m4.translation(-0.03, 0, -20);
+  let translateToRight = m4.translation(0.03, 0, -20);
 
   let matAccumLeft = m4.multiply(translateToLeft, matAccum);
   let matAccumRight = m4.multiply(translateToRight, matAccum);
@@ -257,6 +317,48 @@ const CreateSurfaceData = () => {
   return { vertexList, textureList };
 };
 
+const CreateSphereData = (radius) => {
+  const vertexList = [];
+  const textureList = [];
+  const splines = 20;
+
+  const maxU = Math.PI;
+  const maxV = 2 * Math.PI;
+  const stepU = maxU / splines;
+  const stepV = maxV / splines;
+
+  const getU = (u) => {
+    return u / maxU;
+  };
+
+  const getV = (v) => {
+    return v / maxV;
+  };
+
+  for (let u = 0; u <= maxU; u += stepU) {
+    for (let v = 0; v <= maxV; v += stepV) {
+      const x = radius * Math.sin(u) * Math.cos(v);
+      const y = radius * Math.sin(u) * Math.sin(v);
+      const z = radius * Math.cos(u);
+
+      vertexList.push(x, y, z);
+      textureList.push(getU(u), getV(v));
+
+      const xNext = radius * Math.sin(u + stepU) * Math.cos(v + stepV);
+      const yNext = radius * Math.sin(u + stepU) * Math.sin(v + stepV);
+      const zNext = radius * Math.cos(u + stepU);
+
+      vertexList.push(xNext, yNext, zNext);
+      textureList.push(getU(u + stepU), getV(v + stepV));
+    }
+  }
+
+  return {
+    verticesSphere: vertexList,
+    texturesSphere: textureList,
+  };
+};
+
 /* Initialize the WebGL context. Called from init() */
 function initGL() {
   let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
@@ -275,6 +377,10 @@ function initGL() {
   shProgram.iTextureCoords = gl.getAttribLocation(prog, "textureCoords");
   shProgram.iTextureU = gl.getUniformLocation(prog, "textureU");
 
+  sphere = new Model("Sphere");
+  const { verticesSphere, texturesSphere } = CreateSphereData(10);
+  sphere.BufferData(verticesSphere, texturesSphere);
+
   surface = new Model("Surface");
   const { vertexList, textureList } = CreateSurfaceData();
   surface.BufferData(vertexList, textureList);
@@ -289,6 +395,8 @@ function initGL() {
   );
 
   loadTexture();
+
+  loadSphereTexture();
 
   gl.enable(gl.DEPTH_TEST);
 }
@@ -359,15 +467,31 @@ function init() {
     return;
   }
 
+  // get elements for 3D render
   const eyeSeparationInput = document.getElementById("eyeSeparation");
   const convergenceInput = document.getElementById("convergence");
   const fovInput = document.getElementById("fov");
   const nearInput = document.getElementById("near");
 
+  // get custom audio sphere coords
+  const xPositionInput = document.getElementById("xPosition");
+  const yPositionInput = document.getElementById("yPosition");
+  const zPositionInput = document.getElementById("zPosition");
+
+  const cutoffFrequencyInput = document.getElementById("cutoffFrequency");
+
+  // add eventListeners for 3D params
   eyeSeparationInput.addEventListener("input", draw);
   convergenceInput.addEventListener("input", draw);
   fovInput.addEventListener("input", draw);
   nearInput.addEventListener("input", draw);
+
+  // add eventListeners for inputs custom audio sphere coords
+  xPositionInput.addEventListener("input", draw);
+  yPositionInput.addEventListener("input", draw);
+  zPositionInput.addEventListener("input", draw);
+
+  cutoffFrequencyInput.addEventListener("input", draw);
 
   spaceball = new TrackballRotator(canvas, draw, 0);
 
@@ -376,6 +500,59 @@ function init() {
   } else {
     console.log("Device orientation not supported");
   }
+
+  // get audio element
+  audio = document.getElementById("audio");
+
+  // add eventListener for audio pause
+  audio.addEventListener("pause", () => {
+    audioContext.resume();
+  });
+
+  // add eventListener for audio play
+  audio.addEventListener("play", () => {
+    if (!audioContext) {
+      // Create an AudioContext instance
+      audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+      audioSource = audioContext.createMediaElementSource(audio);
+
+      // Create a panner
+      audioPanner = audioContext.createPanner();
+      audioPanner.panningModel = "HRTF"; // Set the panning model
+      audioPanner.distanceModel = "linear"; // Set the distance model
+
+      // Create a highpass filter
+      audioFilter = audioContext.createBiquadFilter();
+      audioFilter.type = "highpass";
+      // Set the cutoff frequency
+      audioFilter.frequency.value = cutoffFrequencyInput.value;
+
+      // Connect the audio source to the filter, and the filter to the audio context destination
+      audioSource.connect(audioPanner);
+      audioPanner.connect(audioFilter);
+      audioFilter.connect(audioContext.destination);
+
+      audioContext.resume();
+    }
+  });
+
+  // get element of filter
+  let filterOn = document.getElementById("filterCheckbox");
+
+  // add eventListener for change filter checkbox
+  filterOn.addEventListener("change", function () {
+    if (filterOn.checked) {
+      audioPanner.disconnect();
+      audioPanner.connect(audioFilter);
+      audioFilter.connect(audioContext.destination);
+    } else {
+      audioPanner.disconnect();
+      audioPanner.connect(audioContext.destination);
+    }
+  });
+
+  audio.play();
 
   reDraw();
 }
@@ -389,11 +566,25 @@ const loadTexture = () => {
   const image = new Image();
   image.crossOrigin = "anonymous";
   image.src =
-    "https://www.the3rdsequence.com/texturedb/download/200/texture/jpg/1024/stone+tile+grass-256x256.jpg";
+    "https://www.the3rdsequence.com/texturedb/download/212/texture/jpg/1024/concrete+wall+decoration-64x64.jpg";
 
   image.addEventListener("load", () => {
     texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  });
+};
+
+const loadSphereTexture = () => {
+  const image = new Image();
+  image.crossOrigin = "anonymous";
+  image.src = "./sphere-texture.jpg";
+
+  image.addEventListener("load", () => {
+    textureSphere = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, textureSphere);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
